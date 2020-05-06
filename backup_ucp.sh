f62cef9ab9937687039b62aca8c458bd331e7d42
ada mancini <adab3ta@gmail.com>
2020-05-06 13:39:45 -0400

    update service for ucp 3.2

--- backup_ucp.sh
+++ backup_ucp.sh
@@ -6,23 +6,30 @@ function error_exit {
   exit "${2:-1}"  ## Return a code specified by $2 or 1 by default.
 }
 
-[[ -z $UCP_URL ]] && error_exit "you must specify a UCP URL to backup from"
+# [[ -z $UCP_URL ]] && error_exit "you must specify a UCP URL to backup from"
 [[ -z $UCP_USER ]] && error_exit "you must specify a UCP User with admin privileges"
 [[ ! -f /run/secrets/password ]] && error_exit "you must mount a docker secret with your admin password in /run/secrets/password; see 'docker secrets' usage."
 
+
 UCP_PASSWORD="$(cat /run/secrets/password)"
-UCP_ID=$(docker inspect $(docker ps -aq --filter=name=ucp-agent | head -n 1) | jq -r '.[].Config.Env[]' | grep UCP_INSTANCE_ID | cut -d "=" -f 2)
-UCP_VERSION=$(docker inspect $(docker ps -aq --filter=name=ucp-agent | head -n 1) | jq -r '.[].Config.Env[]' | grep IMAGE_VERSION | cut -d "=" -f 2)
+UCP_CLUSTER_ID=$(docker info --format '{{ .Swarm.Cluster.ID }}')
+UCP_VERSION=$(docker inspect --format='{{ index .Config.Labels "com.docker.ucp.version"}}' ucp-controller)
 
-
-echo "Performing UCP backup against cluster at $UCP_URL with id $UCP_ID."
-docker run --rm -i \
+echo "Performing UCP backup against cluster at $UCP_URL with id $UCP_CLUSTER_ID."
+docker container run --rm -i\
   --name ucp \
   --log-driver none \
   -v /var/run/docker.sock:/var/run/docker.sock \
-  docker/ucp:$UCP_VERSION backup \
+  docker/ucp:"${UCP_VERSION}" backup \
   --debug \
-  --id $UCP_ID \
-  --passphrase $UCP_PASSWORD \
+  --id "${UCP_CLUSTER_ID}" \
+  --passphrase "${UCP_PASSWORD}" \
   > "/backup/$(date --iso-8601)-$(hostname)-ucp-backup.tar"
 
+echo "Rotate backups"
+find /backup/*-ucp-backup.tar -exec ls -lh {} +;
+
+echo "Found backup tarballs older than ${MAX_AGE} days"
+find /backup/*-ucp-backup.tar -mtime +"${MAX_AGE}" -exec ls -lh {} +;
+
+find /backup/*-ucp-backup.tar -mtime +"${MAX_AGE}" -delete
